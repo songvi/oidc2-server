b9bf3a0eb8785bd3648d842a73de294b79e4f750
songvi <songvi2025@gmail.com>
2017-07-30 19:49:04 +0200

    add actions on account

--- web/index.php
+++ web/index.php
@@ -6,5 +6,9 @@ use Symfony\Component\Debug\Debug;
 require_once __DIR__.'/../vendor/autoload.php';
 Debug::enable();
 $app = require __DIR__.'/../src/app.php';
-require __DIR__.'/../config/dev.php';
-$app->run();
+//require __DIR__.'/../config/dev.php';
+
+// create an http foundation request implementing OAuth2\RequestInterface
+$request = OAuth2\HttpFoundationBridge\Request::createFromGlobals();
+$app['request'] = $request;
+$app->run($request);
